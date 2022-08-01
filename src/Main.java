6c9d601c62287f2f474ef51b852752e5c9127f92
VitaliyKlochko <89vklochko89@gmail.com>
2022-08-01 23:31:25 +0300

    Задания выполнены

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,7 @@ public class Main {
         float BoxOneWeight = 78.2f;
         float BoxTwoWeight = 82.7f;
         float AllBoxWeight = BoxOneWeight + BoxTwoWeight;
+
         System.out.println("Общий вес боксеров = " + AllBoxWeight + " кг " );
 
         float DifferenceBoxWeight = BoxOneWeight - BoxTwoWeight;
@@ -38,6 +39,64 @@ public class Main {
         System.out.println("Общий вес коктейля = " + KGAllWeight + " кг ");
 
 // Задание 4
+
+        int BoxWeightOutKG = 7;
+        int Weight250OutGRAMM = 250;
+        int DayWeightOut250 = (BoxWeightOutKG * 1000) / Weight250OutGRAMM;
+        System.out.println("Потеря веса в 250 грамм в день за = " + DayWeightOut250 + " дней ");
+
+        int Weight500OutGRAMM = 500;
+        int DayWeightOut500 = (BoxWeightOutKG * 1000) / Weight500OutGRAMM;
+        System.out.println("Потеря веса в 500 грамм в день за = " + DayWeightOut500 + " дней ");
+
+        int OnAverageDay = (DayWeightOut250 + DayWeightOut500)/2;
+        System.out.println("Дней в среднем = " + OnAverageDay + " дней ");
+
+// Задание 5
+        int Maria = 67760;
+        int Denis = 83690;
+        int Kris = 76230;
+
+        double Maria10 = (Maria *0.1) + Maria;     // при int  теряет (округляет) 6 на конце значения
+        double Denis10 = (Denis*0.1)+Denis;
+        double Kris10 = (Kris*0.1)+Kris;
+
+        //System.out.println("Мария получает на 10% больше = " + Maria10);
+        //System.out.println("Денис получает на 10% больше = " + Denis10);
+        //System.out.println("Кристина получает на 10% больше = " + Kris10);
+
+        double Maria365 = Maria10 * 12;
+        //System.out.println("Мария за год ПОСЛЕ увеличения получает -  " + Maria365);
+        double Denis365 = Denis10 * 12;
+        //System.out.println("Денис за год ПОСЛЕ увеличения получает -  " + Denis365);
+        double Kris365 = Kris10 * 12;
+        //System.out.println("Кристина за год ПОСЛЕ увеличения получает -  " + Kris365);
+
+        //до увеличения
+
+        int DoMaria365 = Maria * 12;
+        //System.out.println("Мария за год  ДО увеличения получает -  " + DoMaria365);
+        int DoDenis365 = Denis * 12;
+        //System.out.println("Денис за год  ДО увеличенияЯ получает -  " + DoDenis365);
+        int DoKris365 = Kris * 12;
+        //System.out.println("Кристина за год  ДО увеличения получает -  " + DoKris365);
+
+        //Разница
+
+        double RazMaria = Maria365 - DoMaria365;
+        double RazDenis = Denis365 - DoDenis365;
+        double RazKris = Kris365 - DoKris365;
+        //System.out.println("Raznica -  " + RazMaria);
+
+        System.out.println("Маша теперь получает " + Maria10 + " рублей. Годовой доход вырос на " + RazMaria );
+        System.out.println("Денис теперь получает " + Denis10 + " рублей. Годовой доход вырос на " + RazDenis );
+        System.out.println("Кристина теперь получает " + Kris10 + " рублей. Годовой доход вырос на " + RazKris );
+
+
+
+
+
+
 
 
 
