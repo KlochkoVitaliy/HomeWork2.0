21e4a38a14a33d28607bd838f1f3c1b053c4d5a3
VitaliyKlochko <89vklochko89@gmail.com>
2022-08-01 22:27:37 +0300

    Задание 3 выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!!");
+// Задание 1
+
+        //var a =1;
+        byte b = 2;
+        int c = 3;
+        long d = 4L;
+        short f = 5;
+
+        float e = 5.2f;
+        double g = 3.1;
+
+// Задание 2
+
+        float BoxOneWeight = 78.2f;
+        float BoxTwoWeight = 82.7f;
+        float AllBoxWeight = BoxOneWeight + BoxTwoWeight;
+        System.out.println("Общий вес боксеров = " + AllBoxWeight + " кг " );
+
+        float DifferenceBoxWeight = BoxOneWeight - BoxTwoWeight;
+        System.out.println("Разница между весами боксеров = " + DifferenceBoxWeight + " кг ");
+
+ // Задание 3
+
+        int Banana = 5;
+        int WeightBanana = 80;
+        int Milk = 200;
+        int WeightMilk = 105*2;
+        int IceCreamPlom = 2;
+        int IceCreamWeight = 100;
+        int Egg = 4;
+        int EggWeight = 70;
+
+        int AllWeight =(Banana * WeightBanana) + (Milk * WeightMilk/200) + (IceCreamPlom * IceCreamWeight) + (Egg *EggWeight );
+        System.out.println("Общий вес коктейля = " + AllWeight + " грамм ");
+        float  KGAllWeight = AllWeight / 1000f;
+        System.out.println("Общий вес коктейля = " + KGAllWeight + " кг ");
+
+// Задание 4
+
+
+
+
+
+
     }
 }
